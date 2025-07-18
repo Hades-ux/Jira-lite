40a1a97c170b04d12aaf54b73c6316696cd96920
Hades <vishal1807singh@gmail.com>
2025-07-18 22:52:22 +0530

    list

---
+++ src/components/List.jsx
@@ -0,0 +1,78 @@
+import { FaSearch } from "react-icons/fa"
+
+
+const List = () => {
+
+    const tasks = [
+        {
+            id:1,
+            Summary:"Fix Bug",
+            Assignee: "Davis",
+            Status:"On going",
+            Comment:"Not able to log  in",
+            category:"Bug",
+            dueDate:"20-07-2025",
+            Priorty:"High",
+            Created:"01-07-2025",
+            Update:"",
+            Report:"Manager"
+        }
+    ]
+
+  return (
+    <div className="w-full p-5">
+
+        {/*  Search list */}
+        <div className="border border-gray-300 rounded-sm flex items-center p-3 gap-2 w-60">
+            <FaSearch/>
+            <input type="text"
+            placeholder="Search List..."
+            className="outline-none bg-transparent "
+            />
+        </div>
+
+        {/* Task Table */}
+<div className=" overflow-auto mt-5">
+
+
+        <table className=" border border-gray-300 w-full mt-5" >
+            <thead className=" bg-gray-100">
+                <tr>
+                    <th className="border px-3 py-2"> Summary  </th>
+                    <th className="border px-3 py-2"> Status   </th>
+                    <th className="border px-3 py-2"> Assignee </th>
+                    <th className="border px-3 py-2"> Category </th>
+                    <th className="border px-3 py-2"> Comment  </th>
+                    <th className="border px-3 py-2"> Priority </th>
+                    <th className="border px-3 py-2"> Created  </th>
+                    <th className="border px-3 py-2"> Update   </th>
+                    <th className="border px-3 py-2"> Reporter </th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task) =>(
+                    <tr key = {task.id}>
+                    
+                    <td className=" border px-3 py-2">{task.Summary}</td>
+                    <td className=" border px-3 py-2">{task.Status}</td>
+                    <td className=" border px-3 py-2">{task.Assignee}</td>
+                    <td className=" border px-3 py-2">{task.category}</td>
+                    <td className=" border px-3 py-2">{task.Comment}</td>
+                    <td className=" border px-3 py-2">{task.Priorty}</td>
+                    <td className=" border px-3 py-2">{task.Created}</td>
+                    <td className=" border px-3 py-2">{task.Update}</td>
+                    <td className=" border px-3 py-2">{task.Report}</td>
+
+                    </tr>
+
+                ))}
+            </tbody>
+
+        </table>
+        </div>
+
+    </div>
+  )
+}
+
+export default List
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,78 +1,14 @@
-import { FaBell, FaUserCircle, FaCog, FaGlobe, FaList, FaClipboard, FaCalendar, FaShareAlt, FaRegDizzy, FaBatteryFull } from "react-icons/fa"
-import Button from "../components/Button"
+import Navbar from "../components/Navbar"
+import List from "../components/List"
 
 const projects = () => {
   return (
-    <div className='h-[100vh] w-[100vw]'>
-        <div className=' border-b border-gray-300 h-12 w-full flex justify-around items-center'>
-            <h1>Jira-logo</h1>
-            <div className='h-[100%] flex gap-10 items-center'>
-            <input 
-            type="text"
-            placeholder='Search project...'
-            className=' border-1 border-gray-400 rounded-sm h-[70%] w-150 px-5 outline-none focus:ring-2 focus:ring-indigo-600 hidden sm:block' />
-            <button
-            className='bg-indigo-500 text-white rounded-sm w-30 h-[70%] cursor-pointer hover:bg-indigo-600'>+ Creat</button>
-
-            </div>
-
-            <div className='flex items-center gap-4 text-xl'>
-
-                <FaBell className="text-gray-700"/>
-                <FaCog className="text-gray-700"/>
-                <FaUserCircle className="text-gray-700"/>
-
-            </div>
-        </div>
-
-
-    {/* Navigation Button */}
-
-
-        <div className="h-30 w-full border-b border-gray-300 bg-purple-100 flex">
-
-          <h1 className="p-3">Project</h1>
-
-          <div className="flex items-end-safe">
-            <Button
-            lable = "Summary"
-            icon = {<FaGlobe/>}
-            onclick={""}
-            />
-            <Button
-            lable = "List"
-            icon = {<FaList/>}
-            />
-
-            <Button
-            lable="Board"
-            icon={<FaClipboard/>}
-            />
-
-            <Button
-            lable="Calender"
-            icon={<FaCalendar/>}
-            />
-          </div>
-
-        <div className="relative p-3 flex gap-2 items-center">
-          <button
-          className="p-2 rounded-sm border cursor-pointer hover:scale-105">
-            <FaShareAlt/>
-          </button>
-
-          <button
-          className="p-2 rounded-sm border cursor-pointer hover:scale-105">
-            <FaBatteryFull/>
-          </button>
-
-          <button
-          className="p-2 rounded-sm border cursor-pointer hover:scale-105">
-            <FaRegDizzy/>
-          </button>
-        </div>
-        </div>
-
+    <div className="flex flex-col ">
+    
+    <Navbar/>
+     <div className="mt-10 px-4">
+      <List/>
+     </div>
     </div>
   )
 }
